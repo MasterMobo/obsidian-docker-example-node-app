ead3d8697ed21d96f2403968b502edc121740b81
MasterMobo <mastermobo101@gmail.com>
2023-11-12 17:07:36 +0700

    Hello World fix

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 
 router.get("/", (req, res) => {
-    res.send("Hello World! You are cruel")
+    res.send("Hello World!")
 })
 
 router.post("/user", createNewUser)
